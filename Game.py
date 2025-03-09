51ea07cfc55c8926f40ee71489db57c3d0874d92
Fernando Luis R. Fernando <fernandoluisr.fernando28@gmail.com>
2025-03-09 21:35:43 +0800

    screen color

--- Game.py
+++ Game.py
@@ -3,5 +3,14 @@ import pygame, sys
 pygame. init()
 screen = pygame.display.set_mode((400,500))
 clock = pygame.time.Clock()
-test_surface = pygame.surface((100,200))
- 
+test_surface = pygame.Surface((100,200))
+
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            sys.exit()
+    screen.fill((174,215,70))
+    screen.blit(test_surface,(200,250))    
+    pygame.display.update()
+    clock.tick(60)
